8b71bd7744a052c87a9b2478f2543e47a58af155
Yugp72 <ugp56229@gmail.com>
2025-11-19 21:34:42 -0800

    port change

--- backend/internal/transport/http/router.go
+++ backend/internal/transport/http/router.go
@@ -42,7 +42,7 @@ func NewRouter(d Deps) *gin.Engine {
 	r.Use(gin.Recovery(), gin.Logger())
 
 	r.Use(func(c *gin.Context) {
-		c.Header("Access-Control-Allow-Origin", "http://localhost:3000")
+		c.Header("Access-Control-Allow-Origin", "http://localhost:5137")
 		c.Header("Access-Control-Allow-Credentials", "true")
 		c.Header("Access-Control-Allow-Headers", "Content-Type, Authorization")
 		c.Header("Access-Control-Allow-Methods", "GET,POST,PATCH,DELETE,OPTIONS")
